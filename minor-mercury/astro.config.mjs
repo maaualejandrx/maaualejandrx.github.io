436b5e7a5b06b66a73f4a58bb69ef68be8dba744
maaualejandrx <alexsantiflores03@gmail.com>
2026-06-17 20:26:49 -0600

    git deploy 3

--- minor-mercury/astro.config.mjs
+++ minor-mercury/astro.config.mjs
@@ -1,8 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://maaualejandrx.github.io',
-  base: '/LuzDental', // <-- ESTA LÍNEA ES CRUCIAL
-  integrations: [tailwind()],
+  site: 'https://github.io',
+  base: '/',
 });
