982e4a1f9d53af02024fd91fbaefa14fa50a0a26
maaualejandrx <alexsantiflores03@gmail.com>
2026-06-17 20:41:34 -0600

    chore: instalar integracion de tailwind en minor-mercury

--- minor-mercury/astro.config.mjs
+++ minor-mercury/astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // <-- Volvemos a importar Tailwind
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://github.io',
-  base: '/',
+  site: 'https://maaualejandrx.github.io', // <-- Tu dominio de GitHub Pages
+  base: '/LuzDental',                     // <-- La subruta de tu repositorio para que cargue el CSS
+  integrations: [tailwind()],             // <-- Activamos Tailwind en el proceso de compilación
 });
